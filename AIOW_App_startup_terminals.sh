656a057b312cbffe14ca9f0f0d1aef89c98063a2
Diego <lldiegon@gmail.com>
2022-02-12 06:27:05 +0100

    Added Aiow app startup script

---
+++ AIOW_App_startup_terminals.sh
@@ -0,0 +1,9 @@
+guake --new-tab=$HOME/Projects/AIOW/mobile
+guake --rename-current-tab=React_Native_Instance
+guake --execute-command="npx react-native start --reset-cache --verbose"
+
+
+guake --new-tab=$HOME/Projects/AIOW/mobile
+guake --rename-current-tab=AIOW_App
+guake --execute-command="yarn ios"
+
