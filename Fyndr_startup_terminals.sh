33f5616881fa75b4d1dcee0de7ec69e6736cf91a
Diego <lldiegon@gmail.com>
2022-02-12 05:56:06 +0100

    Initial Commit

---
+++ Fyndr_startup_terminals.sh
@@ -0,0 +1,6 @@
+guake --new-tab=$HOME/Projects/fyndr/Backend
+guake --rename-current-tab=Fyndr_Instance
+guake --execute-command="docker-compose up"
+
+guake --new-tab=$HOME/Projects/fyndr/Backend
+guake --rename-current-tab=Fyndr_Backend
